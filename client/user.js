46cbbcff799d9891bd495567482a36ef5fb90322
Dmytro V. Dogadailo <dvd@dogada.org>
2016-02-16 00:11:31 +0200

    Switched user detail to tagsView

--- client/user.js
+++ client/user.js
@@ -6,10 +6,4 @@ var tflow = require('tflow')
 var {ui} = require('coect')
 var store = require('./store')
 
-exports.detail = function(ctx) {
-  
-  var flow = tflow([
-    () => ui.getData(ctx, 'data', next => store.user.get(ctx.path, next), flow),
-    (data) => ui.renderTags(data, flow)
-  ], Site.error)
-}
+exports.detail = ui.tagsView(store.user)
